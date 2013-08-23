f5187ae68f17ae47b88a8f98c77d437accd19557
Alexander Finkenberger <alex+github@weltregie.org>
2013-08-23 19:33:47 +0200

    first code commit

---
+++ checktls.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from socket import *
+from dns import resolver
+
+def get_mx(domain):
+    for mx in resolver.query(domain, 'MX'):
+        # get first MX server
+        first_mx = mx.to_text().split()[1][:-1]
+    return first_mx
+
+domains = ('finkenberger.org', 'gmx.de', 'web.de', 'hotmail.com', 'yahoo.com',
+           'aol.com', 'gmail.com', 't-online.de')
+
+tls_supported = []
+tls_not_supported = []
+
+for domain in domains:
+    domain_mx = get_mx(domain)
+    try:
+        client_socket = socket(AF_INET, SOCK_STREAM)
+        client_socket.connect((domain_mx, 25))
+        recv = client_socket.recv(1024)
+        client_socket.send('EHLO example.com\r\n')
+        recv1 = client_socket.recv(1024)
+        if 'STARTTLS' in recv1:
+            tls_supported.append(domain)
+            print domain, domain_mx, "unterstützt"
+        else:
+            tls_not_supported.append(domain)
+            print domain, domain_mx, "NICHT unterstützt"
+    except error, msg:
+        print domain, domain_mx, 'FEHLER:', msg
+
+
+print
+print "TLS wird unterstützt von", len(tls_supported), "Servern"
+print "TLS wird NICHT unterstützt von", len(tls_not_supported), "Servern"
+
