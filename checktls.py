54dda6813794b68fda5042c4f81cb2245175aacf
weltregie <alex+github@weltregie.org>
2019-01-17 11:00:58 +0100

    return table of mx servers; list TLS support status in table

--- checktls.py
+++ checktls.py
@@ -32,10 +32,26 @@ def check_mx_tls(domain_mx):
 tls_supported = []
 tls_not_supported = []
 
-for domain in domains:
-    domain_mx = get_mx(domain)[0]
-    if check_mx_tls(domain_mx):
-        print domain, domain_mx, "TLS"
-    else:
-        print domain, domain_mx
+result_table=[]
+result_tls_supported = []
+result_tls_not_supported = []
 
+for domain in domains:
+    domain_mx_servers = get_mx_servers_by_priority(domain)
+    if domain_mx_servers:
+        for mx_server in domain_mx_servers:
+            result_list = [domain]     # first element of list: domain
+	    result_list.append(mx_server)
+	    tls_result = check_mx_tls(mx_server)
+            if tls_result:
+                result_list.append('TLS supported')
+                result_tls_supported.append(domain)
+                break
+            if tls_result == False:
+                result_list.append('TLS not supported')
+                result_tls_not_supported.append(domain)
+		break
+            if tls_result == None:
+                result_list.append('no answer')
+        result_table.append(result_list)
+        print result_list[0].ljust(22), result_list[1].ljust(37), result_list[2].ljust(20)
